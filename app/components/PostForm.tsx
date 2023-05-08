2979c251ab655e2b704926f3c7d313700e97f2bc
Draxerra <michael.tugby5@gmail.com>
2023-05-08 19:24:24 +0100

    Remove <PostFormErrorBoundary>

--- app/components/PostForm.tsx
+++ app/components/PostForm.tsx
@@ -1,25 +1,17 @@
-'use client'
-
 import { createPost } from "@/app/actions/posts";
 import Button from "@/app/components/Button";
-import PostFormErrorBoundary from "@/app/components/PostFormErrorBoundary";
 
 /** Seems to force pages to dynamic? */
 export default function PostForm() {
   return (
-    <PostFormErrorBoundary>
-      {(error, reset) => (
-        <form action={createPost} className='flex flex-col gap-y-4' onSubmit={reset}>
-          <label className='flex flex-col gap-y-2'>
-            <span className='font-bold'>Add a comment</span>
-            <textarea className='p-4 shadow-xl' name='message' rows={4} />
-            {error && <span className='text-red-700 font-bold text-sm'>{error.message}</span>}
-          </label>
-          <Button className='self-start' type='submit'>
-            Add Comment
-          </Button>
-        </form>
-      )}
-    </PostFormErrorBoundary>
+    <form action={createPost} className='flex flex-col gap-y-4'>
+      <label className='flex flex-col gap-y-2'>
+        <span className='font-bold'>Add a comment</span>
+        <textarea className='p-4 shadow-xl' name='message' rows={4} />
+      </label>
+      <Button className='self-start' type='submit'>
+        Add Comment
+      </Button>
+    </form>
   )
 }
--- app/error.tsx
+++ app/error.tsx
@@ -12,7 +12,6 @@ export default function Error({ error, reset }: ErrorProps) {
     <main className='p-8'>
       <section aria-labelledby='error-heading' className='space-y-4 text-center'>
         <h1 className='text-5xl font-bold' id='error-heading'>Oops, something went wrong!</h1>
-        <p className='text-lg'><strong>Error:</strong> {error.message}</p>
         <Button onClick={reset}>Try again?</Button>
       </section>
     </main>
