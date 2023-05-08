1094b09f68b8fbf36fa1a71c023a4a52f6db1071
Draxerra <michael.tugby5@gmail.com>
2023-05-08 21:42:19 +0100

    Add basic form error handling to <PostForm>

--- app/actions/posts.ts
+++ app/actions/posts.ts
@@ -3,6 +3,10 @@
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 
+export type FormReturnData = {
+  errors: Record<string, string>;
+}
+
 export async function updatePostLikes(id: number, likes: number) {
   await sql`UPDATE posts SET likes = ${likes} WHERE id=${id}`;
   /** Currently bugged? Doesn't seem to trigger a revalidation */
@@ -10,15 +14,16 @@ export async function updatePostLikes(id: number, likes: number) {
   revalidatePath(`/post/${id}`);
 }
 
-export async function createPost(data: FormData) {
+export async function createPost(data: FormData): Promise<FormReturnData> {
   const message = data.get('message');
   if (typeof message !== 'string') {
-    throw new Error('Message is invalid.');
+    return { errors: { message: 'Message is invalid.' } };
   }
   if (message.length === 0) {
-    throw new Error('Message is required.');
+    return { errors: { message: 'Message is required.' } };
   }
   await sql`INSERT INTO posts (message, likes) VALUES (${message}, 0)`;
   /** Ditto */
   revalidatePath('/');
+  return { errors: {} };
 }
--- app/components/PostForm.tsx
+++ app/components/PostForm.tsx
@@ -1,13 +1,24 @@
-import { createPost } from "@/app/actions/posts";
+'use client'
+
+import { useState } from "react";
+import { type FormReturnData, createPost } from "@/app/actions/posts";
 import Button from "@/app/components/Button";
 
 /** Seems to force pages to dynamic? */
 export default function PostForm() {
+  const [formErrors, setFormErrors] = useState<FormReturnData['errors']>({});
   return (
-    <form action={createPost} className='flex flex-col gap-y-4'>
+    <form
+      action={async(data) => {
+        const { errors } = await createPost(data);
+        setFormErrors(errors);
+      }}
+      className='flex flex-col gap-y-4'
+    >
       <label className='flex flex-col gap-y-2'>
         <span className='font-bold'>Add a comment</span>
         <textarea className='p-4 shadow-xl' name='message' rows={4} />
+        {formErrors.message && <span className='text-red-700 text-sm font-bold'>{formErrors.message}</span>}
       </label>
       <Button className='self-start' type='submit'>
         Add Comment
